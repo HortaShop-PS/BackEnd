a68336a18369b26ab8170c0b14dda8ee868d0aa7
Gustavo Santos <gustavogsantos2013@gmail.com>
2025-04-21 12:23:23 -0300

    Requisicoes para editar perfil

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,35 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { User } from '../entities/user.entity';
+import { Controller, Get, Post, Patch, Delete, Body, Param } from "@nestjs/common"
+import { UsersService } from './users.service'
+import { CreateUserDto, UpdateUserDto, UpdatePasswordDto, UserResponseDto } from "../dto/user.dto"
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  async findAll(): Promise<User[]> {
-    return this.usersService.findAll();
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
+    return this.usersService.create(createUserDto)
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
-    return this.usersService.findOne(id);
+  findOne(@Param('id') id: string): Promise<UserResponseDto> {
+    return this.usersService.findOne(+id)
+  }
+
+  @Patch(":id")
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UserResponseDto> {
+    return this.usersService.update(+id, updateUserDto)
+  }
+
+  @Patch(":id/password")
+  updatePassword(@Param('id') id: string, @Body() updatePasswordDto: UpdatePasswordDto): Promise<{ message: string }> {
+    return this.usersService
+      .updatePassword(+id, updatePasswordDto)
+      .then(() => ({ message: "Senha atualizada com sucesso" }))
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<{ message: string }> {
+    return this.usersService.remove(+id)
   }
 }
